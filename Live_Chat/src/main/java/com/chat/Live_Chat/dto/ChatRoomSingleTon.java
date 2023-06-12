fcf1bd75f343f875f42c4730432de5a280cc1321
JunSang Jang <85791363+JunSang1121@users.noreply.github.com>
2023-06-12 14:11:53 +0900

    Update ChatRoomSingleTon.java

--- Live_Chat/src/main/java/com/chat/Live_Chat/dto/ChatRoomSingleTon.java
+++ Live_Chat/src/main/java/com/chat/Live_Chat/dto/ChatRoomSingleTon.java
@@ -16,6 +16,5 @@ public class ChatRoomSingleTon {
     }
 
 
-    // 싱글톤 다시 공부해야함. 왜 static을 사용하는지, 왜 hashmap이 아니라 ConcurrentHashMap을 사용하는지, getinstance할때는 왜 static을 사용하는지 다시 공부.
-    //        고정 메모리를 통해 객체를 1개만 사용하기 위해서,   멀티 스레드를 사용하여 1:n을 가능하게,    static을 사용하는 이유, 고정 메모리로 객체가 변경되지 않게
+    
 }
